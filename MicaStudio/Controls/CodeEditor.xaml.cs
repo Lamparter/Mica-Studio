f6849d1d67c960695a1cf0b2973ff7efa9591597
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-03-19 22:03:29 +0100

    Add project files.

---
+++ MicaStudio/Controls/CodeEditor.xaml.cs
@@ -0,0 +1,178 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using TextMateSharp.Grammars;
+using TextMateSharp.Internal.Grammars;
+using TextMateSharp.Registry;
+using TextMateSharp.Themes;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI;
+using WinUIEditor;
+using static System.Net.Mime.MediaTypeNames;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MicaStudio.Controls
+{
+	public sealed partial class CodeEditor : UserControl
+	{
+		public CodeEditor()
+		{
+			this.InitializeComponent();
+
+			
+		}
+
+		private void addLine(int line, string text)
+		{
+			ScintillaEditor.Editor.GotoLine(line);
+			ScintillaEditor.Editor.AddText(text.Length, text);
+			ScintillaEditor.Editor.NewLine();
+		}
+
+
+		public async void OpenFile(StorageFile file)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			ScintillaEditor.Editor.ClearAll();
+			using (IRandomAccessStream stream = await file.OpenReadAsync())
+			{
+				using (DataReader reader = new DataReader(stream))
+				{
+					await reader.LoadAsync((uint)stream.Size);
+					string fileContent = reader.ReadString(reader.UnconsumedBufferLength);
+					ScintillaEditor.Editor.SetText(fileContent);
+				}
+			}
+			ScintillaEditor.ResetLexer();
+			ScintillaEditor.ApplyDefaultsToDocument();
+
+			stopwatch.Stop();
+			Debug.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+
+			await SyntaxHighlight(file.FileType);
+
+			ScintillaEditor.Editor.Modified += Editor_Modified; // start dynamic highlighting
+		}
+
+		private void Editor_Modified(Editor sender, ModifiedEventArgs args)
+		{
+			int startLine = (int)ScintillaEditor.Editor.LineFromPosition(args.Position); // gets position of first line modified
+			int endLine = (int)ScintillaEditor.Editor.LineFromPosition(args.Position + args.Length); // gets position of last line modified
+			// gets all lines between range start and end including end line
+			int[] linePositionsModified = Enumerable.Range(startLine, endLine - startLine + 1).ToArray();
+
+			// loop through all modified lines and re syntax highlight them
+			foreach (int linePosition in linePositionsModified)
+			{
+				string line = ScintillaEditor.Editor.GetLine(linePosition);
+				ITokenizeLineResult result = grammar.TokenizeLine(line);
+				parseTokens(result, line, linePosition, registry);
+			}
+		}
+
+		// Maps a textmate colour to a scintilla style key
+		private Dictionary<int, int> colorToScintillaStyle = new();
+		private IGrammar grammar;
+		private Registry registry;
+
+		public async Task SyntaxHighlight(string extension)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			RegistryOptions options = new RegistryOptions(ThemeName.DarkPlus);
+		    registry = new Registry(options);
+			grammar = registry.LoadGrammar(options.GetScopeByExtension(extension)); // parameter is initial scope name
+
+			IStateStack? ruleStack = null; // important for state of token, without it multi line comments wont work
+
+			for (int i = 0; i < ScintillaEditor.Editor.LineCount; i++)
+			{
+				string line = ScintillaEditor.Editor.GetLine(i);
+				ITokenizeLineResult result = grammar.TokenizeLine(line, ruleStack, TimeSpan.MaxValue);
+				ruleStack = result.RuleStack;
+
+				if (result.Tokens.Count() == 0) break; // return if no tokens
+				parseTokens(result, line, i, registry);
+			}
+
+			stopwatch.Stop();
+			Debug.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+		}
+
+		private void parseTokens(ITokenizeLineResult result, string line, long linePosition, Registry registry)
+		{
+			Theme theme = registry.GetTheme();
+			foreach (IToken token in result.Tokens)
+			{
+				int startIndex = (token.StartIndex > line.Length) ? line.Length : token.StartIndex;
+				int endIndex = (token.EndIndex > line.Length) ? line.Length : token.EndIndex;
+				List<ThemeTrieElementRule> themeRules = theme.Match(token.Scopes);
+
+				foreach (ThemeTrieElementRule themeRule in themeRules)
+				{
+					// get position of current line i
+					long linePos = ScintillaEditor.Editor.PositionFromLine(linePosition);
+
+					// Register foreground colour to a scintilla style if it does not exist
+					if (!colorToScintillaStyle.ContainsKey(themeRule.foreground))
+					{
+						var color = Convert.ToInt32(theme.GetColor(themeRule.foreground).Replace("#", ""), 16);
+						int key = colorToScintillaStyle.Count + 1;
+						// IMPORTANT: Define a style with a unique KEY mapped to a colour, we will use it to highlight tokens
+						ScintillaEditor.Editor.StyleSetFore(key, color);
+
+						// add it to hashmap so we can retrieve it
+						colorToScintillaStyle[themeRule.foreground] = key;
+					}
+
+					// start styling from token position by using line position and index of token
+					ScintillaEditor.Editor.StartStyling(linePos + startIndex, 0);
+					// USE the style which we defined earlier for foreground on the token
+					// the scintilla style KEYS are in the hashmap
+					ScintillaEditor.Editor.SetStyling(endIndex - startIndex, colorToScintillaStyle[themeRule.foreground]);
+					/*Debug.WriteLine(
+						"      - Matched theme rule: " +
+						"[bg: {0}, fg:{1}, fontStyle: {2}]",
+						theme.GetColor(themeRule.background),
+						theme.GetColor(themeRule.foreground),
+						themeRule.fontStyle);*/
+				}
+			}
+		}
+
+		public void clear() => ScintillaEditor.Editor.ClearAll();
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			ScintillaEditor.Editor.AnnotationSetText(0, "test");
+			ScintillaEditor.Editor.AnnotationVisible = AnnotationVisible.Indented;
+
+			ScintillaEditor.Editor.IndicSetStyle(8, IndicatorStyle.FullBox);
+			ScintillaEditor.Editor.IndicSetFore(8, 0x0000ff);
+			ScintillaEditor.Editor.IndicatorCurrent = 8;
+			ScintillaEditor.Editor.IndicatorFillRange(0, 7);
+		}
+	}
+}
---
+++ MicaStudio/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using WinRT.Interop;
+using WinUIEditor;
+using WinUIEx;
+using System.Collections.ObjectModel;
+using Microsoft.UI.Dispatching;
+using System.Diagnostics;
+using System.Xml.Linq;
+using TextMateSharp.Grammars;
+using TextMateSharp.Registry;
+using TextMateSharp.Themes;
+using TextMateSharp.Internal.Themes.Reader;
+using TextMateSharp.Internal.Types;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MicaStudio
+{
+	public record TreeNode(string Name, string Path, bool isFolder, ObservableCollection<TreeNode> Children);
+	/// <summary>
+	/// An empty window that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainWindow : WindowEx
+    {
+		private ObservableCollection<TreeNode> files = new();
+        public MainWindow()
+        {
+            this.InitializeComponent(); 
+            this.ExtendsContentIntoTitleBar = true;
+			this.SetTitleBar(AppTitleBar);
+		}
+
+		private async void OpenFile_Click(object sender, RoutedEventArgs e)
+		{
+			var picker = new FileOpenPicker();
+			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+			InitializeWithWindow.Initialize(picker, hwnd);
+			picker.FileTypeFilter.Add("*");
+			var file = await picker.PickSingleFileAsync();
+
+			if (file != null)
+			{
+
+			}
+		}
+
+		private async void OpenFolder_Click(object sender, RoutedEventArgs e)
+		{
+			var picker = new FolderPicker();
+			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+			InitializeWithWindow.Initialize(picker, hwnd);
+			picker.FileTypeFilter.Add("*");
+			var folder = await picker.PickSingleFolderAsync();
+
+			if (folder != null)
+			{
+				Editor.clear();
+				files.Clear();
+				var rootFolderNode = new TreeNode(folder.Name, folder.Path, true, new ObservableCollection<TreeNode>());
+				files.Add(rootFolderNode);
+			}
+		}
+
+		private async Task LoadFolderContentAsync(TreeNode node)
+		{
+			string currentFolderPath = node.Path;
+
+			try
+			{
+				// Get subfolders in the current directory
+				var subFolders = Directory.GetDirectories(currentFolderPath);
+				foreach (var subFolder in subFolders)
+				{
+					var subFolderName = Path.GetFileName(subFolder);
+					node.Children.Add(new TreeNode(subFolderName, subFolder, true, new ObservableCollection<TreeNode>()));
+				}
+
+				// Get files in the current directory
+				var files = Directory.GetFiles(currentFolderPath);
+				foreach (var file in files)
+				{
+					var fileName = Path.GetFileName(file);
+					node.Children.Add(new TreeNode(fileName, file, false, new ObservableCollection<TreeNode>()));
+				}
+			}
+			catch (Exception ex)
+			{
+				// Handle exceptions like unauthorized access, path not found, etc.
+				// You can log the exception or show a message to the user
+				Debug.WriteLine(ex.Message);
+			}
+		}
+
+		/*private async Task LoadFolderContentAsyncWinRT(TreeNode node)
+		{
+			if (!node.isFolder) return;
+			var currentFolder = node.StorageItem as StorageFolder;
+			var subFolders = await currentFolder.GetFoldersAsync();
+			var files = await currentFolder.GetFilesAsync();
+			foreach (var subFolder in subFolders)
+			{
+				node.Children.Add(new TreeNode(subFolder.Name, subFolder, true, new ObservableCollection<TreeNode>()));
+			}
+
+			// Add files one by one
+			foreach (var file in files)
+			{
+				node.Children.Add(new TreeNode(file.Name, file, false, new ObservableCollection<TreeNode>()));
+			}
+		}*/
+
+		// Load items when TreeViewNode expanding
+		private async void ExplorerTreeView_Expanding(TreeView sender, TreeViewExpandingEventArgs args)
+		{
+			var node = args.Item as TreeNode;
+			if (node.isFolder && node.Children.Count == 0)
+			{
+				await LoadFolderContentAsync(args.Item as TreeNode);
+			}
+		}
+
+		private async void ExplorerTreeView_SelectionChanged(TreeView sender, TreeViewSelectionChangedEventArgs args)
+		{
+			var node = args.AddedItems[0] as TreeNode;
+			if (!node.isFolder)
+			{
+				Editor.OpenFile(await StorageFile.GetFileFromPathAsync(node.Path));
+			}
+		}
+	}
+}
